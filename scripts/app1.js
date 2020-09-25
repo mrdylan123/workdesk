fbaa394baac0d6efa13636d0139af118adace303
Dylan Martens <dylanhs123@hotmail.com>
2020-09-25 13:27:31 +0200

    Rendering of single events now correctly implemented

--- scripts/app1.js
+++ scripts/app1.js
@@ -1,38 +1,53 @@
-var werkbonnen;
+var werkbonnen = [];
 
 var eventColorPalette = ["#6ba292", "#FFCC42", "#ff9b71", "#dd614a"];
-var counter = 0;
 
 $.ajax({url: "https://5f6c8a6834d1ef0016d583ab.mockapi.io/werkbonnen", success: function(result){    
     result.forEach(element => {
-        console.log(element);
+        //console.log(element);
+        werkbonnen.push(element);
 
         var $newEvent = $("<div class='event'></div>");
         var uniqueClassName = "event" + element.werkbonnummer;
         $newEvent.addClass(uniqueClassName);
         $newEvent.append("<p>" + element.titel + "</p>");
+        $newEvent.append("<p>" + element.startdatum + " - " + element.einddatum + "</p>");
 
-        var startHourClass = ".time" + parseInt(element.startdatum); 
-        var startPosition = $(startHourClass).position();
+        
+        var marginTop = parseFloat($(".hiddenDiv").css("margin-top")); // Get second child, as first child has more margin-top than the others
 
+        var timePosition = $(".time" + parseFloat(element.startdatum)).position();
 
-        var height = (parseInt(element.einddatum) - parseInt(element.startdatum)) * 15; // Each hour is 15vh (margin-top: 5vh and padding-bottom: 10vh)
+        var height = (parseFloat(element.einddatum) - parseFloat(element.startdatum)) * 15; // Each hour is 15vh (margin-top: 5vh and padding-bottom: 10vh)
 
+        var dayWidth = $(".daydiv").outerWidth(); // Get width of parent div
+
+        // TODO: Check if there are any other events colliding with this one
+        var eventWidth = dayWidth - 30; // Subtract the padding of the parent div
 
         var color = eventColorPalette[Math.floor((Math.random() * 4))];
-        console.log(color);
+
 
         $newEvent.css({
-                        "top": startPosition.top,
                         "position": "absolute", 
-                        "margin-top": "1.75vh",  // Set margin-top to 1.75vh, just like the horizontal roster lines, as the div's height is 3.5vh, but the text is vertically centered.
+                        "top": timePosition.top + marginTop,
                         "height": height + "vh",
                         "background-color": color,
-                        "width": "85%",
-                        "color": "white"  
-                    });
+                        "width": eventWidth,
+                        "color": "white",
+                        "text-align": "center",
+                        "font-family": "Arial, Helvetica",
+                        "font-size": "14px",
+                        "font-weight": "bold",
+                        "border-radius": "5px" // Rounding of the corners
+        });
+
+
+        console.log($newEvent.css("min-width"));
 
         $(".day1").append($newEvent);
     });
+
+    console.log(werkbonnen);
 
   }});
