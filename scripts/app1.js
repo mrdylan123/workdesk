a4b2337b0c102a3e05dff2d0bb6728ec71b30ee6
Dylan Martens <dylanhs123@hotmail.com>
2020-09-28 16:33:54 +0200

    Page now scrolls to current time and clean up some code

--- scripts/app1.js
+++ scripts/app1.js
@@ -6,7 +6,6 @@ var marginTop = parseFloat($(".hiddenDiv").css("margin-top")); // Get the margin
 
 $.ajax({url: "https://5f6c8a6834d1ef0016d583ab.mockapi.io/werkbonnen", success: function(result){    
     result.forEach(element => {
-        //console.log(element);
         werkbonnen.push(element);
 
         var $newEvent = $("<div class='event'></div>");
@@ -39,7 +38,6 @@ $.ajax({url: "https://5f6c8a6834d1ef0016d583ab.mockapi.io/werkbonnen", success: 
                         "font-size": "14px",
                         "font-weight": "bold",
                         "border-radius": "5px",
-                        // "z-index": "1" // Rounding of the corners
         });
 
 
@@ -61,12 +59,12 @@ function ScrollToCurrentTime() {
 
     // Calculate the height of a minute in the agenda
     var minuteHeight = ($('.time12').offset().top - $('.time11').offset().top) / 60;
-    
+
     // Get the minuteheight of the current time
     var minutePosition = minuteHeight * dt.getMinutes();
 
     // Add the minutes to it's position
-    var timePosition = hourPosition.top + minutePosition;
+    var timePosition = hourPosition.top + minutePosition + marginTop;
 
     var dayWidth = $(".daydiv").outerWidth(); // Get width of day div
 
@@ -74,30 +72,44 @@ function ScrollToCurrentTime() {
     var $currentTimePointer = $("<div class='currentTimePointer'></div>");
     $currentTimePointer.css({
         "position": "absolute", 
-        "top": timePosition + marginTop,
-        "height": "3px",
+        "top": timePosition - 1.5, // Subtract 1.5, to keep it centered (1/2 of div size)
+        "border-top": "#FF3636 solid 3px",
         "width": dayWidth,
-        "background-color": "#FF3636",
-        // "border-radius": "5px", // Rounding of the corners
         "z-index": "1"
     });
 
     var $currentTimeCircle = $("<div class='currentTimeCircle'></div>");
     $currentTimeCircle.css({
         "position": "absolute", 
-        "top": timePosition + marginTop,
-        "height": "12px",
-        "width": "12px",
-        "margin-left": "-6px",
-        "margin-top": "-5px",
+        "top": timePosition - 5, // Subtract 5, to keep it centered (1/2 of div size)
+        "height": "10px",
+        "width": "10px",
+        "margin-left": "-5px",
         "background-color": "#FF3636",
         "border-radius": "50%",
-        // "border-radius": "5px", // Rounding of the corners
         "z-index": "1"
     });
     $(".agendarow").append($currentTimePointer);
     $(".agendarow").append($currentTimeCircle);
-  }
+
+    // Scroll page to current timeframe
+    var pos = timePosition;
+    var elHeight = 3;
+    var windowHeight = $(window).height();
+    var offset;
+
+    if (elHeight < windowHeight) {
+        offset = pos - ((windowHeight / 2) - (elHeight / 2)) + $(".nav").height() + $(".header").height();
+    }
+    else {
+        offset = pos;
+    }
+
+    window.scroll({
+            top: offset,
+            behavior: 'smooth',
+          });
+}
 
 // Call the function as the page is loaded in
-jQuery(window).on('load', ScrollToCurrentTime);
+$(window).on('beforeunload', ScrollToCurrentTime());
